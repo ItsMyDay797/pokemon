acc20c24c619dee081f2e1f7ff03d0e27ace23c8
ItsMyDay797 <m.rus1298@gmail.com>
2022-08-21 22:34:54 +0300

    UI editing

--- pokemon/Controller/ResultViewController.swift
+++ pokemon/Controller/ResultViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Марк Русаков on 19.08.22.
 //
 
-import UIKit
+import SwiftUI
 
 class ResultViewController: UIViewController {
     var name: UILabel!
@@ -17,15 +17,31 @@ class ResultViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        name = UILabel()
-        name.text = "Name"
+        let layout = view.layoutMarginsGuide
         
-        view.addSubview(name)
+        let textLabel = UILabel()
+        textLabel.text = "Pokemon's name"
+        textLabel.frame = CGRect(x: 10, y: 25, width: textLabel.frame.size.width, height: textLabel.frame.size.height)
+        
+        let textLabel1 = UILabel()
+        textLabel1.text = "Pokemon's weight"
+        
+        view.addSubview(textLabel)
+        
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        textLabel.topAnchor.constraint(equalTo: layout.topAnchor).isActive = true
+        textLabel.leadingAnchor.constraint(equalTo: layout.leadingAnchor).isActive = true
+        
+//        textLabel1.translatesAutoresizingMaskIntoConstraints = false
+//        textLabel1.topAnchor.constraint(equalTo: layout.topAnchor).isActive = true
+//        textLabel1.leadingAnchor.constraint(equalTo: layout.leadingAnchor).isActive = true
+
     }
     
 
     
-   
-  
 
 }
+
+
+
